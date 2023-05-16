ca882f7d035b2604714d76c88d4c6596ff5e8870
carlson3912 <jackacarlson3912@gmail.com>
2023-05-16 00:49:58 -0700

    NPC movement version1 --too slow and not the right angle, but there is motion

--- src/Components/MultiPlayer.js
+++ src/Components/MultiPlayer.js
@@ -9,6 +9,16 @@ import idle from '../assets/idle.fbx';
 import walk from '../assets/walking.fbx';
 import {Box, ClickAnimation} from './Misc'
 import { useMemo } from "react";
+
+function calculateWalkVector(facingDirection) {
+  // Calculate the x and y components of the unit vector based on the facing direction
+  const x = Math.cos(facingDirection);
+  const y = Math.sin(facingDirection);
+
+  // Return the unit vector as an object with x and y properties
+  return [x, y ];
+}
+
 function Floor(props) {
     const floorRef = useRef();
     const move = (event) => {
@@ -86,11 +96,13 @@ function AnimatedFBXModel(props) {
       if (!moving){
         setTargetX(x);
         setTargetZ(z);
+        var tempFacing = 0;
         if (x-userX < 0){
-          setFacing(Math.PI + Math.atan((z-userZ)/(x-userX)));
+          tempFacing = Math.PI + Math.atan((z-userZ)/(x-userX));
         } else {
-          setFacing(Math.atan((z-userZ)/(x-userX)));
+          tempFacing = Math.atan((z-userZ)/(x-userX));
         }
+        setFacing(tempFacing);
         setMoving(true);
         if(props.socket){
             props.socket.emit("send_message", {message: [x,z]});
@@ -112,10 +124,11 @@ function AnimatedFBXModel(props) {
           setMoving(false);
         }
         const delta = clock.getDelta() * 3;
+        console.log("****clock delta", delta);
         // console.log(delta2);
-        console.log(delta);
-        // console.log(length);
-        console.log(diffX/length* delta, "delta: ", delta, " at ", userX);
+        // console.log(delta);
+        // // console.log(length);
+        // console.log(diffX/length* delta, "delta: ", delta, " at ", userX);
         setUserX(userX + (diffX / length) * delta);
         setUserZ(userZ + (diffY / length) * delta);
         // setUserX(userX+diffX/length/50); //dividing by length finds the unit vector of the path
@@ -150,16 +163,31 @@ function AnimatedFBXModel(props) {
     );
   }
 function NPC(props){
+    const [realPos, setRealPos] = useState([0,0]);
     const [fbx, setFbx] = useState(null);
     // const [meshRef, setMeshRef] = useState(null);
   
   useEffect(() => {
     new FBXLoader().load(character, setFbx);
   }, []);
-
+  useFrame (({clock}) => {
+    if(props.second){
+      if(props.second[1]){
+        console.log("moving on a frame", realPos, clock.getDelta());
+        const [x,y] = calculateWalkVector(props.second[0]);
+        const diffX = 0.02 * x + realPos[0];
+        const diffY = 0.02 * y + realPos[1];
+        setRealPos([diffX, diffY]);
+      }
+  }
+  });
   useEffect(() => {
     console.log("NPC just moved");
+    setRealPos([props.position[0],props.position[2]]);
   }, [props.postion]);
+  useEffect(() => {
+    console.log("props.second", props.second);
+  }, [props.second]);
 
   if (!fbx) {
     return null;
@@ -179,7 +207,7 @@ function NPC(props){
                 T-Mobile
               </Text>
             </mesh> */}
-             <primitive object={fbx} position={[props.position[0],-1,props.position[2]]}/>
+             <primitive object={fbx} position={[realPos[0],-1,realPos[1]]}/>
              {/* <AnimatedFBXModel url={walk} url2={idle}animationName="walking" isWalking={moving}/> */}
         </>
     )
@@ -187,16 +215,45 @@ function NPC(props){
 }
 
 function Players(props){
+  // const clock = useRef(new THREE.Clock());
     const [dict, setDict] = useState({});
+    const [facing, setFacing] = useState({});
     const [socket, setSocket] = useState(null);
     // const fbx = useLoader(FBXLoader, character);
     // fbx.scale.set(0.01, 0.01, 0.01);
+    // useFrame(({ clock }) => {
+    //   const updates = {};
+    
+    //   for (const key in facing) {
+    //     if (facing.hasOwnProperty(key)) {
+    //       const [x, y] = facing[key];
+    //       if (y) {
+    //         const [userX, userZ] = facing[key];
+    //         const [diffx, diffy] = calculateWalkVector(x);
+    //         var delta = clock.getDelta();
+    //         if (delta > 0.5){
+    //           delta = 0.5;
+    //         }
+    //         delta = 0.02
+    //         console.log("clocl delta", delta);
+    //         const x1 = userX + diffx * delta;
+    //         const x2 = userZ + diffy * delta;
+    //         updates[key] = [x1, x2];
+    //       }
+    //     }
+    //   }
+    
+    //   // Update the state once per frame
+    //   setDict(prevState => ({ ...prevState, ...updates }));
+    // });
+   
+      
     useEffect(() => {
         console.log("Dict after update:", dict);
       }, [dict]);
     useEffect(() => {
         
-        const newSocket = io.connect("http://localhost:3000");
+        const newSocket = io.connect("http://localhost:3002");
         setSocket(newSocket);
 
         newSocket.on("welcome", (data) => {
@@ -216,23 +273,27 @@ function Players(props){
         newSocket.on("rec_message", (data) => {
             const temp = [data[1], data[2]];
             const key = data[0]
-            console.log("socket npc just moved");
-            console.log("ID: ", data[0]);
-            console.log("moving from:", dict);
-            console.log("moving to:", temp);
+            // console.log("socket npc just moved");
+            // console.log("ID: ", data[0]);
+            // console.log("moving from:", dict);
+            // console.log("moving to:", temp);
             setDict(prevState => ({ ...prevState, [key]: temp }));
-        });
-        // newSocket.on("moving_message", (data) => {
-            
-        // });
-      }, []);
+            setFacing(prevState => ({ ...prevState, [key]: [0, false]}));
+        
+      });
+      newSocket.on("moving_message", (data) => {
+        console.log("NPC facing: ", data);
+        setFacing(prevState => ({ ...prevState, [data[0]]: [data[1], true] }));
+        console.log("facing,", facing);
+    });
+    }, []);
     return(
         <>
         <User controlsRef = {props.controlsRef} socket={socket}/>
         {
         Object.entries(dict).map(([key, value], index) => (
          
-                <NPC key={`NPC-${index}`} position={[value[0], 8, value[1]]}/>
+                <NPC key={`NPC-${index}`} position={[value[0], 8, value[1]]} second = {facing[key]}/>
          
        ))}
         </>
