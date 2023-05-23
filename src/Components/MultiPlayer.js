43ac51ce8b9dd81bc99711ba3ce7652b559598cb
carlson3912 <jackacarlson3912@gmail.com>
2023-05-23 15:35:25 -0700

    animations, facing and beginning of loading bar for NPCs

--- src/Components/MultiPlayer.js
+++ src/Components/MultiPlayer.js
@@ -1,89 +1,88 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
-import { Canvas, useFrame, useThree, useLoader } from '@react-three/fiber';
+import React, { useState, useRef, useEffect } from 'react';
+import {useFrame, useLoader } from '@react-three/fiber';
 import * as THREE from 'three';
 import io from 'socket.io-client';
-import { OrbitControls, Text } from '@react-three/drei';
+import { Text } from '@react-three/drei';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import character from '../assets/character.fbx';
 import idle from '../assets/idle.fbx';
 import walk from '../assets/walking.fbx';
-import {Box, ClickAnimation} from './Misc'
-import { useMemo } from "react";
-
-function calculateWalkVector(facingDirection) {
-  // Calculate the x and y components of the unit vector based on the facing direction
-  const x = Math.cos(facingDirection);
-  const y = Math.sin(facingDirection);
-
-  // Return the unit vector as an object with x and y properties
-  return [x, y ];
-}
-
-function Floor(props) {
-    const floorRef = useRef();
-    const move = (event) => {
-      console.log(event.point.x);
-      props.updateLocation(event.point.x, event.point.z);
-    };
-  
-    return (
-      <mesh
-        receiveShadow={true}
-        rotation={[-3.1415/2, 0, 0]}
-        position={[0, -1, 0]}
-        ref={floorRef}
-        onClick={move}
-        layers={(floorRef.current && floorRef.current.layers.enable(0), [])}
-      >
-        <planeBufferGeometry receiveShadow attach="geometry" args={[1000, 1000]} />
-        <meshStandardMaterial receiveShadow attach="material" color="black"  />
-      </mesh>
-    );
-  }
+import {ClickAnimation, Floor} from './Misc'
 
 function AnimatedFBXModel(props) {
-    const fbx = useLoader(FBXLoader, props.url);
-    const fbx2 = useLoader(FBXLoader, props.url2);
-    const mixerRef = useRef();
-    const mixerRef2 = useRef();
+    const [fbx, setFbx] = useState(null);
+    const [fbx2, setFbx2] = useState(null);
     const [mixes, setMixer] = useState(null);
     const [mixes2, setMixer2] = useState(null);
+    const mixerRef = useRef();
+    const mixerRef2 = useRef();
+    useEffect(() => {
+      const loader1 = new FBXLoader();
+      const loader2 = new FBXLoader();
+      loader1.load(idle, (fbx) => {
+        setFbx(fbx);
+      });
   
-    useFrame((state, delta) => {
-      if (props.isWalking && mixes) {
-        mixerRef.current && mixerRef.current.update(delta);
-      }
-      if (!props.isWalking && mixes2) {
+      loader2.load(walk, (fbx) => {
+        setFbx2(fbx);
+      });
+  
+      Promise.all([loader1, loader2]).then(() => {
+        props.loadHandler();
+        console.log('Done loading, ', props.position);
+      });
+  
+    }, []);
+
+    useFrame((state,delta) => {
+      if (props.isWalking && mixes2) {
         mixerRef2.current && mixerRef2.current.update(delta);
+      }
+      if (!props.isWalking && mixes) {
+        mixerRef.current && mixerRef.current.update(delta);
       }
     });
   
     useEffect(() => {
-      const mixer = new THREE.AnimationMixer(fbx);
-      const action = mixer.clipAction(fbx.animations[0]);
-      action.play();
-      setMixer(mixer);
-      mixerRef.current = mixer; // assign the mixer to the ref
-  
-      const mixer2 = new THREE.AnimationMixer(fbx);
-      const action2 = mixer2.clipAction(fbx2.animations[0]);
-      action2.play();
-      setMixer2(mixer2);
-      mixerRef2.current = mixer2; // assign the mixer to the ref
-    }, []);
-  
+      if(fbx){
+        fbx.scale.set(0.01, 0.01, 0.01);
+        const mixer = new THREE.AnimationMixer(fbx);
+        const action = mixer.clipAction(fbx.animations[0]);
+        action.play();
+        setMixer(mixer);
+        mixerRef.current = mixer; // assign the mixer to the ref
+        // assign the mixer to the ref
+      }
+    }, [fbx]);
+    useEffect(() => {
+      if(fbx2){
+        fbx2.scale.set(0.01, 0.01, 0.01);
+        const mixer = new THREE.AnimationMixer(fbx2);
+        const action = mixer.clipAction(fbx2.animations[0]);
+        action.play();
+        setMixer2(mixer);
+        mixerRef2.current = mixer; // assign the mixer to the ref
+        // assign the mixer to the ref
+      }
+    }, [fbx2]);
+    if (!fbx) {
+      return null;
+    }
+    if (!fbx2) {
+      return null;
+    }
     return (
       <group>
-        <primitive object={fbx} />
+        { !props.isWalking?
+          <primitive object={fbx} position = {props.position} rotation = {props.rotation}/>
+        : 
+          <primitive object={fbx2} position = {props.position} rotation = {props.rotation}/>
+        }
       </group>
     );
   }
+
   function User(props) {
-    const clock = new THREE.Clock();
-    clock.start();
-    const walkingSpeed = 5; //decrease to accelerate
-    const fbx = useLoader(FBXLoader, walk);
-    fbx.scale.set(0.01, 0.01, 0.01);
     const meshRef = useRef();
     const [userX, setUserX] = useState(0);
     const [userZ, setUserZ] = useState(0);
@@ -91,8 +90,15 @@ function AnimatedFBXModel(props) {
     const [targetZ, setTargetZ] = useState(0);
     const [moving, setMoving] = useState(false);
     const [facing, setFacing] = useState(3.1415/3);
+    const clock = new THREE.Clock();
+    //Restart the clock when the User is rerendered. Everytime a new move instruction occurs, a rerender is triggered.
+    clock.start();
+    const fbx = useLoader(FBXLoader, walk);
+    fbx.scale.set(0.01, 0.01, 0.01);
     
     function handleClick(x,z){
+      props.controlsRef.current.maxPolarAngle = Math.PI / 2.1; 
+      props.controlsRef.current.minPolarAngle = Math.PI / 9 ;
       if (!moving){
         setTargetX(x);
         setTargetZ(z);
@@ -105,14 +111,13 @@ function AnimatedFBXModel(props) {
         setFacing(tempFacing);
         setMoving(true);
         if(props.socket){
-            props.socket.emit("send_message", {message: [x,z]});
+            props.socket.emit("send_message", [x,z]);
         }
         
       }
   }
-  
+
     useFrame (({camera}) => {
-  
       if (meshRef.current) {
         meshRef.current.lookAt(camera.position);
       }
@@ -124,26 +129,25 @@ function AnimatedFBXModel(props) {
           setMoving(false);
         }
         const delta = clock.getDelta() * 3;
-        console.log("****clock delta", delta);
-        // console.log(delta2);
-        // console.log(delta);
-        // // console.log(length);
-        // console.log(diffX/length* delta, "delta: ", delta, " at ", userX);
-        setUserX(userX + (diffX / length) * delta);
-        setUserZ(userZ + (diffY / length) * delta);
-        // setUserX(userX+diffX/length/50); //dividing by length finds the unit vector of the path
-        // setUserZ(userZ+diffY/length/50);
-
-        props.controlsRef.current.target.set(userX+diffX/length, 0.5, userZ+diffY/length);
-        props.controlsRef.current.maxPolarAngle = Math.PI / 2.1; 
-        props.controlsRef.current.minPolarAngle = Math.PI / 9 ;
+        var fraction = delta / length;
+        if (fraction > 1){
+          setUserX(targetX);
+          setUserZ(targetZ);
+          props.controlsRef.current.target.set(targetX, 0.5, targetZ);
+        }
+        else{
+        setUserX(userX + diffX * fraction);
+        setUserZ(userZ + diffY * fraction);
+        props.controlsRef.current.target.set(userX + diffX * fraction, 0.5, userZ + diffY * fraction);
+        }
       }
     });
+
+   
     return (
       <>
-         <primitive object={fbx} position={[userX,-1,userZ]} rotation ={[0,3.1415/2 - facing,0]}/>
           <Floor updateLocation={handleClick}/> 
-          <AnimatedFBXModel url={walk} url2={idle}animationName="walking" isWalking={moving}/>
+          <AnimatedFBXModel url={walk} url2={idle}animationName="walking" isWalking={moving}position={[userX,-1,userZ]} rotation ={[0,3.1415/2 - facing,0]} loadHandler={props.loadHandler}/>
             <mesh ref={meshRef} position={[userX, 2, userZ]}>
               <Text
                // set the position of the text
@@ -155,48 +159,100 @@ function AnimatedFBXModel(props) {
                 {props.username}
               </Text>
             </mesh>
-          {moving == true? 
+          {moving === true? 
             <ClickAnimation x = {targetX} z ={targetZ}/>
-            :null
+          :
+            null
           }
       </>
     );
   }
+
 function NPC(props){
+  const clock3 = useRef(new THREE.Clock());
     const [realPos, setRealPos] = useState([0,0]);
     const [fbx, setFbx] = useState(null);
-    // const [meshRef, setMeshRef] = useState(null);
+    const [first, setFirst] = useState(null);
+    const [facing, setFacing] = useState(0);
+    const [walkVector, setWalkVector] = useState(null);
+    const [positionSnap, setPositionSnap] = useState(null);
+    const [distance, setDistance] = useState(0);
+    const meshRef = useRef(null);
+    const fbx3 = useLoader(FBXLoader, character);
+    fbx3.scale.set(0.01, 0.01, 0.01);
   
   useEffect(() => {
     new FBXLoader().load(character, setFbx);
   }, []);
-  useFrame (({clock}) => {
-    if(props.second){
-      if(props.second[1]){
-        console.log("moving on a frame", realPos, clock.getDelta());
-        const [x,y] = calculateWalkVector(props.second[0]);
-        const diffX = 0.02 * x + realPos[0];
-        const diffY = 0.02 * y + realPos[1];
-        setRealPos([diffX, diffY]);
+  useFrame (({camera}) => {
+    if(meshRef != null){
+      if(meshRef.current) {
+        meshRef.current.lookAt(camera.position);
       }
   }
+    //if there is a walk command
+    if(props.position){
+      if(props.position !== realPos && walkVector != null){
+        //to make no skipping the end
+        const constip = clock3.current.getDelta() * 3;
+        const newX = constip * walkVector[0] ;
+        const newY = constip * walkVector[1] ;
+        const length = Math.sqrt(newX*newX+newY*newY); 
+        setDistance(distance - length);
+        if (distance < 0.5) {
+          setRealPos(props.position);
+        }
+        else{
+          setRealPos([newX + realPos[0], newY+ realPos[1]]);
+        }
+      }
+  }
+  
   });
   useEffect(() => {
-    console.log("NPC just moved");
-    setRealPos([props.position[0],props.position[2]]);
-  }, [props.postion]);
+      setRealPos(props.position);
+      setFirst(props.postion);
+    }, []);
   useEffect(() => {
-    console.log("props.second", props.second);
-  }, [props.second]);
+    const x = props.position[0];
+    const z = props.position[1];
+    const userX = realPos[0];
+    const userZ = realPos[1];
+    var tempFacing;
+    if (x-userX < 0){
+      tempFacing = Math.PI + Math.atan((z-userZ)/(x-userX));
+    } else {
+      tempFacing = Math.atan((z-userZ)/(x-userX));
+    }
+    setFacing(tempFacing);
+
+    if(props.position != null && props.position != first){
+      clock3.current.start();
+    //everyime NPC needs to walk to a new location
+    var real = realPos;
+    //put him at the right start place
+   
+   
+    if(positionSnap != null){
+        setRealPos(positionSnap);
+        real = positionSnap;
+    }
+    setPositionSnap(props.position);
+    //calculate unit vector
+    const diffX = props.position[0] -real[0];
+    const diffY = props.position[1] -real[1];
+    var length = Math.sqrt(diffX*diffX+diffY*diffY); 
+    setDistance(length);
+    setWalkVector([(props.position[0] - real[0])/length, (props.position[1] - real[1])/length]);
+  }
+  }, [props.position]);
 
   if (!fbx) {
     return null;
   }
-
-    fbx.scale.set(0.01, 0.01, 0.01);
     return(
         <>
-            {/* <mesh ref={meshRef} position={[props.position[0],2,props.position[2]]}>
+            <mesh ref={meshRef} position={[realPos[0],2,realPos[1]]}>
               <Text
                // set the position of the text
               fontSize={1} // set the font size
@@ -204,98 +260,68 @@ function NPC(props){
               anchorX="center" // set the horizontal alignment
               anchorY="middle" // set the vertical alignment
               >
-                T-Mobile
+                {props.socketCon}
               </Text>
-            </mesh> */}
-             <primitive object={fbx} position={[realPos[0],-1,realPos[1]]}/>
-             {/* <AnimatedFBXModel url={walk} url2={idle}animationName="walking" isWalking={moving}/> */}
+            </mesh>
+          <AnimatedFBXModel isWalking={realPos != props.position} position={[realPos[0],-1,realPos[1]]} rotation ={[0,3.1415/2 - facing,0]} loadHandler={props.loadHandler}/>
         </>
     )
 
 }
 
 function Players(props){
-  // const clock = useRef(new THREE.Clock());
     const [dict, setDict] = useState({});
-    const [facing, setFacing] = useState({});
     const [socket, setSocket] = useState(null);
-    // const fbx = useLoader(FBXLoader, character);
-    // fbx.scale.set(0.01, 0.01, 0.01);
-    // useFrame(({ clock }) => {
-    //   const updates = {};
-    
-    //   for (const key in facing) {
-    //     if (facing.hasOwnProperty(key)) {
-    //       const [x, y] = facing[key];
-    //       if (y) {
-    //         const [userX, userZ] = facing[key];
-    //         const [diffx, diffy] = calculateWalkVector(x);
-    //         var delta = clock.getDelta();
-    //         if (delta > 0.5){
-    //           delta = 0.5;
-    //         }
-    //         delta = 0.02
-    //         console.log("clocl delta", delta);
-    //         const x1 = userX + diffx * delta;
-    //         const x2 = userZ + diffy * delta;
-    //         updates[key] = [x1, x2];
-    //       }
-    //     }
-    //   }
-    
-    //   // Update the state once per frame
-    //   setDict(prevState => ({ ...prevState, ...updates }));
-    // });
-   
-      
+    const [playersLoaded, setPlayersLoaded] = useState(0);
+    const [setup, setSetup] = useState(false);
     useEffect(() => {
-        console.log("Dict after update:", dict);
-      }, [dict]);
-    useEffect(() => {
-        
         const newSocket = io.connect("http://localhost:3002");
         setSocket(newSocket);
-
         newSocket.on("welcome", (data) => {
-            console.log("welcomed: ", data);
-            setDict(data);
+          setDict(data);
+          setSetup(true);
+          console.log("setup is True ", playersLoaded);
         });
-        
         newSocket.on("removePlayer", (data) => {
-            console.log("bye bye: ", data);
-            setDict(prevState => {
+          setDict(prevState => {
               const { [data]:_, ...newState } = prevState;
               return newState;
             });
-      
-          });
-
-        newSocket.on("rec_message", (data) => {
-            const temp = [data[1], data[2]];
-            const key = data[0]
-            // console.log("socket npc just moved");
-            // console.log("ID: ", data[0]);
-            // console.log("moving from:", dict);
-            // console.log("moving to:", temp);
-            setDict(prevState => ({ ...prevState, [key]: temp }));
-            setFacing(prevState => ({ ...prevState, [key]: [0, false]}));
-        
+        });
+        newSocket.on("init_message", (data) => {
+          setDict(prevState => ({ ...prevState, [data[0]]: [0,0] }));
       });
-      newSocket.on("moving_message", (data) => {
-        console.log("NPC facing: ", data);
-        setFacing(prevState => ({ ...prevState, [data[0]]: [data[1], true] }));
-        console.log("facing,", facing);
-    });
+        newSocket.on("moving_message", (data) => {
+          const temp = [data[1], data[2]];
+          const key = data[0]
+          setDict(prevState => ({ ...prevState, [key]: temp }));
+      });
+      
     }, []);
+
+    function loadHandler() {
+      setPlayersLoaded(prevPlayersLoaded => {
+        const curr = prevPlayersLoaded + 1;
+        const dictLength = Object.keys(dict).length + 1;
+        if (curr === dictLength && setup) {
+          console.log("Fully fully loaded: ", dictLength, dict);
+        } else {
+          console.log("step loaded: ", curr, " / ", dictLength);
+        }
+        return curr;
+      });
+    }
     return(
         <>
-        <User username = {props.username} controlsRef = {props.controlsRef} socket={socket}/>
-        {
-        Object.entries(dict).map(([key, value], index) => (
-         
-                <NPC key={`NPC-${index}`} position={[value[0], 8, value[1]]} second = {facing[key]}/>
-         
-       ))}
+      
+        {setup && (
+  <>
+    <User username = {props.username} controlsRef = {props.controlsRef} socket={socket} loadHandler={loadHandler}/>
+    {Object.entries(dict).map(([key, value], index) => (
+      <NPC key={`NPC-${key}`} position={value} socketCon={key} loadHandler={loadHandler} />
+    ))}
+  </>
+)}
         </>
     )
 }
